1f50fbddd702358da98d3c94e697ba253618a336
mendasriharinaidu <mendasriharinaidu@gmail.com>
2026-07-27 22:58:30 +0530

    Create QPSK_Mod.v

---
+++ QPSK_Mod.v
@@ -0,0 +1,26 @@
+module QPSK_Mod(clk,in,i,q);
+input clk;
+input [1:0]in;
+output reg signed[7:0]i,q;
+
+always@(*)
+case(in)
+2'b00:begin
+   i<=8'd1;
+   q<=8'd1;
+   end
+2'b01:begin
+   i<=-8'd1;
+   q<=8'd1;
+   end   
+2'b10:begin
+   i<=8'd1;
+   q<=-8'd1;
+   end
+2'b11:begin
+   i<=-8'd1;
+   q<=-8'd1;
+   end
+endcase
+
+endmodule
