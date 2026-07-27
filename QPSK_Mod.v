f87704a90023c94fa42b0b00dc8d16a7194d23cb
mendasriharinaidu <mendasriharinaidu@gmail.com>
2026-07-27 23:15:36 +0530

    Update QPSK_Mod.v

--- QPSK_Mod.v
+++ QPSK_Mod.v
@@ -6,21 +6,17 @@ output reg signed[7:0]i,q;
 always@(*)
 case(in)
 2'b00:begin
-   i<=8'd1;
-   q<=8'd1;
-   end
+      i=8'd1; q=8'd1;
+      end
 2'b01:begin
-   i<=-8'd1;
-   q<=8'd1;
-   end   
+      i=-8'd1; q=8'd1;
+      end   
 2'b10:begin
-   i<=8'd1;
-   q<=-8'd1;
-   end
+      i<=8'd1; q<=-8'd1;
+      end
 2'b11:begin
-   i<=-8'd1;
-   q<=-8'd1;
-   end
+      i<=-8'd1; q<=-8'd1;
+      end
 endcase
 
 endmodule
